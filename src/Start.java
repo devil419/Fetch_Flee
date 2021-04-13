fc1dd3589c7ab8210a98b9672515e431e02524b3
Shamim Ahmed <cseshamim47@gmail.com>
2021-04-13 15:56:51 +0600

    shamim 13 april registration complete

--- src/Start.java
+++ src/Start.java
@@ -1,72 +1,87 @@
-
-import Registration.*;
-
 import java.util.Scanner;
+/*
+BUG LIST GOES HERE
+*/
 
-public class Start
-{
-    public static void main(String[] args)
-    {
+/*
+        1. Login
+        2. Registration
+        3. TravelLocation
+           i) Starting From
+           ii) Destination
+        4. Choose Root
+            i) Bus
+           ii) Train
+          iii) Plane
+           iv) Ship
+        5. Cost Calculation
+        6. Confirmation Payment
+        7. Print Ticket
+*/
+
+public class Start {
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // LOGIN START //
 
-        User userObj = new User("fullName","username","password","mailAddress","mobile");
-        User userObj2 = new User("Roy","username","password","mailAddress","mobile");
+        try{ Login login = new Login(); }
+        catch (Exception ignored){}
+        Utility.pauseWithMessage();
+        Utility.cls();
 
-        System.out.println("----------------------------------------------------------------");
-        System.out.println("|                      WELCOME TO FETCH FLEE                     | ");
-        System.out.println("-----------------------------------------------------------------");
+        // LOGIN END //
 
-        System.out.println("                   Choose Your Option --------->");
-        System.out.println("                      1. Registration/Login");
-        System.out.println("                      2. Purchase A Ticket");
-        System.out.println("                      3. Verify a Ticket");
-        System.out.println("                      4. Contact Us");
-
-        Registration userEmptyObj = new User();
-        Admin adminEmptyObj = new Admin(100);
-
-        adminEmptyObj.insertUser(userObj);
-        adminEmptyObj.insertUser(userObj2);
-
-        System.out.print("                     Enter Full Name : ");
-        String fullName = input.nextLine();
-
-        System.out.print("                     Enter Username : ");
-        String username= input.nextLine();
-
-        String password;
-        for(;;){
-            System.out.print("                     Enter Password : ");
-            password = input.nextLine();
-
-            System.out.print("                     Enter Password again : ");
-            String checkPW = input.nextLine();
-            if(checkPW.equals(password)) break;
-            System.out.println("                     Password didn't match!! \n");
-        }
-        System.out.print("                      Enter Mail Address : ");
-        String mailAddress= input.nextLine();
-        System.out.print("                      Enter Mobile : ");
-        String mobile= input.nextLine();
+        // RESERVATION START //
+        Reservation reservation = new Reservation(1);
+        System.out.print("Enter Any Key to Continue.......");
+        input.nextLine();
+        Utility.cls();
         
-        userObj.setFullName(fullName);
-        userObj.setUsername(username);
-        userObj.setPassword(password);
-        userObj.setMailAddress(mailAddress);
-        userObj.setMobile(mobile);
+        
+        System.out.println("              ----------------------------------------------------");
+        System.out.println("              |                    CHOOSE ROOT                    |");
+        System.out.println("              ----------------------------------------------------");
+        
+        
+        System.out.println("                              Choose Your Root --> ");
+        System.out.println("                                     1. Bus");
+        System.out.println("                                     2. Train");
+        System.out.println("                                     3. Plane");
+        System.out.println("                                     4. Ship");
+        System.out.print("                                Enter Your Choice : ");
+        int root = input.nextInt();
+        Utility.cls(); 
+        
+        ChooseRoot chooseRoot = new ChooseRoot(root);
+        System.out.print("Enter Any Key to Continue.......");
+        input.nextLine();
+        input.nextLine();
+        Utility.cls();
+        
+        CostCalculation cc = new CostCalculation();
+        
+        
+        System.out.println("              ----------------------------------------------------");
+        System.out.println("             |                      PAYMENT                        |");
+        System.out.println("              ----------------------------------------------------");
+        
 
-        System.out.println(userObj.getFullName());
-        System.out.println(userObj.getUsername());
-        System.out.println(userObj.getPassword());
-        System.out.println(userObj.getMailAddress());
-        System.out.println(userObj.getMobile());
-
-        System.out.println("------------");
-        adminEmptyObj.showAllUser();
-
+        System.out.println("                               Choose Payment Method -->");
+        System.out.println("                                       1. Bkash");
+        System.out.println("                                       2. Rocket");
+        System.out.println("                                       3. Credit/Visa");
+        System.out.print("                                  Enter Your Choice : ");
+        int choice = input.nextInt();
+        //Utility.cls();
+        Payment p = new Payment(choice);
+        System.out.println("Enter Any Key to Continue.......");
+        input.nextLine();
+        input.nextLine();
+        Utility.cls();
+        PrintTicket pt = new PrintTicket();
+        //System.out.println("Enter Any Key to Continue.......");
+        //input.nextLine();
     }
-
-
-
 
 }
